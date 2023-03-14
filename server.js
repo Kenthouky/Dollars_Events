2b5d690112d7c8fef1c9c3085f2bd63bc26e0ada
Dollars Kenthouky <dollarskenthouky@gmail.com>
2023-03-14 15:22:31 +0100

    new

---
+++ server.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv')
+dotenv.config();
+
+
+
+//const bodyParser = require('body-parser');
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Require Notes Routes
+require('./app/routes/note.routes.js')(app);
+
+//app.use(bodyParser.urlencoded({ extended: true}))
+
+//app.use(bodyParser.json())
+
+mongoose.connect("mongodb+srv://dollars:currency@cluster0.gh4jygf.mongodb.net/?retryWrites=true&w=majority", { useNewUrlParser: true, useUnifiedTopology: true })
+	.then(() => {
+		console.log("Connected to MongoDB");
+
+	app.get('/', (req, res) => {
+		res.json({"message": "Welcome to Dollars Event Management System, the BIGGEST IN TOWN!"});
+	});
+
+	app.listen(port, () => {
+		console.log(`Server listening at http://localhost:${port}`);
+	});
+})
+.catch((err) => {
+	console.error(err);
+});
+//app.get('/', (req, res) => {
+//	res.json({"message": "Welcome to Dollars Event Management System, the BIGGEST IN TOWN!"});
+//});
+
+//app.listen(3000, () => {
+//	console.log("Server is listening on port 3000");
+//});
